e17fec4acce243644a71d69f5c106e0e2c5372b8
Mino Zang <minoscc+dev@gmail.com>
2019-07-18 17:24:10 +0800

    format

---
+++ prettier.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+	printWidth: 97,
+	useTabs: true,
+	tabWidth: 4,
+	semi: true,
+	singleQuote: true,
+	jsxSingleQuote: true,
+	jsxBracketSameLine: false,
+	bracketSpacing: false,
+	endOfLine: 'lf',
+	trailingComma: 'es5',
+};
